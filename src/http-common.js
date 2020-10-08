ae6acab31e70fe27de0560ac329ae9708b84605d
dyaksa <diasnour0395@gmail.com>
2020-10-08 08:28:28 +0700

    add server

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://zpayment-api.herokuapp.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
